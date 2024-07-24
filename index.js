40abe859bc157baa1dffb4fa3d5b2f37916b318f
Mr-R-e-X <souvikhazra151@gmail.com>
2024-07-24 11:33:45 +0530

    assifnment done

---
+++ index.js
@@ -0,0 +1,44 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+
+import express from "express";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import {
+  handleGame,
+  handleLogin,
+  handlePost,
+  handleSignUp,
+  renderLogin,
+  renderSignUp,
+} from "./user.controller.js";
+import { auth } from "./middleware/auth.js";
+import { generateRandomNumber } from "./middleware/generateRandomNumber.js";
+
+const app = express();
+
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", auth, generateRandomNumber, handleGame);
+app.post("/guess", handlePost);
+
+app.get("/login", renderLogin);
+app.get("/signup", renderSignUp);
+
+app.post("/login", handleLogin);
+app.post("/signup", handleSignUp);
+
+export default app;
